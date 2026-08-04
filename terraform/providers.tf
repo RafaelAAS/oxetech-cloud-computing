e8a9ae05fa81fae8d64470f0a48204194253907a
Rafael Anacleto <rafael.anacleto@hotmail.com>
2026-08-04 18:00:49 -0300

    fix: usa ubuntu-20.04 e provider azurerm 3.85.0 para evitar GPG expirado

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "= 3.85.0"
     }
   }
   required_version = ">= 1.0"
@@ -10,11 +10,9 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-
+  subscription_id            = var.subscription_id
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  tenant_id                  = var.tenant_id
   skip_provider_registration = true
 }
