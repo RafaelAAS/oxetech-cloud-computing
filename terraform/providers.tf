b64d75b3a25c2e634b0b3b3a4c52c25eee30d2ec
Rafael Anacleto <rafael.anacleto@hotmail.com>
2026-08-04 17:52:36 -0300

    fix: usa terraformrc para contornar verificacao GPG expirada

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.116.0"
+      version = "~> 3.100"
     }
   }
   required_version = ">= 1.0"
@@ -15,4 +15,6 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+
+  skip_provider_registration = true
 }
